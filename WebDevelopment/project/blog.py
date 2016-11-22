1ba479e8ed54993bc56df4ea1db789d85f41c3fd
Thanh Le <thanhle@Thanhs-MacBook-Pro.local>
2016-11-22 14:04:37 -0800

    Basic blog

--- WebDevelopment/project/blog.py
+++ WebDevelopment/project/blog.py
@@ -15,14 +15,75 @@ def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-class BaseHandler(webapp2.RequestHandler):
-    def render(self, template, **kw):
-        self.response.out.write(render_str(template, **kw))
-
+class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-class Rot13(BaseHandler):
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '</b><br>')
+    response.out.write(post.content)
+
+class MainPage(BlogHandler):
+  def get(self):
+      self.write('Hello, Udacity!')
+
+##### blog stuff
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+class BlogFront(BlogHandler):
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('front.html', posts = posts)
+
+class PostPage(BlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+class NewPost(BlogHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+
+
+###### Unit 2 HW's
+class Rot13(BlogHandler):
     def get(self):
         self.render('rot13-form.html')
 
@@ -47,7 +108,7 @@ EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
-class Signup(BaseHandler):
+class Signup(BlogHandler):
 
     def get(self):
         self.render("signup-form.html")
@@ -82,7 +143,7 @@ class Signup(BaseHandler):
         else:
             self.redirect('/unit2/welcome?username=' + username)
 
-class Welcome(BaseHandler):
+class Welcome(BlogHandler):
     def get(self):
         username = self.request.get('username')
         if valid_username(username):
@@ -90,7 +151,12 @@ class Welcome(BaseHandler):
         else:
             self.redirect('/unit2/signup')
 
-app = webapp2.WSGIApplication([('/unit2/rot13', Rot13),
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/unit2/rot13', Rot13),
                                ('/unit2/signup', Signup),
-                               ('/unit2/welcome', Welcome)],
+                               ('/unit2/welcome', Welcome),
+                               ('/blog/?', BlogFront),
+                               ('/blog/([0-9]+)', PostPage),
+                               ('/blog/newpost', NewPost),
+                               ],
                               debug=True)
